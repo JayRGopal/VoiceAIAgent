3745e65890951c4c0565402bfeb54a5e99d8780c
danielechoi0 <daniel_e_choi@brown.edu>
2025-03-24 06:22:13 -0400

    Added ollama prompt engineering

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from p2p import p2p_argument, call_number  # Import the functions from helpers.py
+from p2p import p2p_argument, call_number, summarize_with_ollama  # Import the functions from helpers.py
 
 def main():
     # Receive doctor's arguments
     argument = p2p_argument()
     print(argument)
-    
+
+    new_prompt = summarize_with_ollama(argument)
     number = input("Please input the phone number of who you wish for me to call: ")
 
     # Call number and return transcript
-    call_number(number, argument)
+    call_number(number, new_prompt)
 
 if __name__ == "__main__":
     main()
